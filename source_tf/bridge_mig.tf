3f2f4c52c555294eb40d1bc0972f3c47929607c7
iSaxena-g <saxenaishita@google.com>
2025-10-08 16:39:32 +0000

    delete source_tf

--- source_tf/bridge_mig.tf
+++
@@ -1,9 +0,0 @@
-module "apigee-x-bridge-mig" {
-  for_each    = var.apigee_instances
-  source      = "./modules/apigee-x-bridge-mig"
-  project_id  = module.service-project.project_id
-  network     = module.shared-vpc.network.id
-  subnet      = module.shared-vpc.subnet_self_links[local.subnet_region_name[each.value.region]]
-  region      = each.value.region
-  endpoint_ip = module.apigee-x-core.instance_endpoints[each.key]
-}
--- source_tf/glb.tf
+++
@@ -1,8 +0,0 @@
-module "mig-l7xlb" {
-  source          = "./modules/mig-l7xlb"
-  project_id      = module.service-project.project_id
-  name            = "apigee-xlb"
-  backend_migs    = [for _, mig in module.apigee-x-bridge-mig : mig.instance_group]
-  ssl_certificate = [module.nip-development-hostname.ssl_certificate]
-  external_ip     = module.nip-development-hostname.ip_address
-}
